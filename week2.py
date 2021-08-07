c597230c58606734adfff8c7db94477293998621
aruncbkumar <cbakumar@yahoo.com>
2021-08-07 18:28:07 -0400

    first commit

---
+++ week2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 21:08:45 2021
+
+@author: Arun Kumar
+"""
+
+print("My first python code for 'Python for Everyone' Class")
+
+print("Sum of 1 and 1 is", 1+1)
